61b28a9e2990e96e1dece4fbeec8ef2db6964cce
roylee0704 <roylee0704@gmail.com>
2015-08-03 23:18:51 +0800

    initial commit

---
+++ src/session/SessionBean.java
@@ -0,0 +1,12 @@
+package session;
+
+import javax.ejb.Stateless;
+
+/**
+ * Created by roylee on 8/3/15.
+ */
+@Stateless(name = "SessionEJB")
+public class SessionBean {
+    public SessionBean() {
+    }
+}
